fe8d60bcb848c609a470781e97448caf9981ee19
Mr Booi <ayabongabooi2@gmail.com>
2018-04-05 14:40:07 +0200

    changes on calculate bill

--- js/calculate-bill.js
+++ js/calculate-bill.js
@@ -4,59 +4,70 @@ var calculateBtn = document.querySelector(".calculateBtn");
 var billTotalElement = document.querySelector(".billTotal");
 //get a reference to the billString
 var billStringElement = document.querySelector(".billString");
-//create the function that will be called when the calculate button is pressed
-//  * this function should read the string value entered - split it on a comma.
-//  * loop over all the entries in the the resulting list
-//  * check if it is a call or an sms and add the right amount to the overall total
-//  * once done looping over all the entries - display the total onto the screen in the billTotal element
 
-function calculateBill(billString){
- //var checkbill = billStringElement.value;
-//console.log(checkbill);
- var  billoutput = billString.split(",");
-var priceSms =0;
-var priceCall=0;
-var priceTotal =0;
-  for (var i = 0; i <  billoutput.length; i++) {
+
+function billCalculate() {
+  var priceSms = 0;
+  var priceCall = 0;
+  var priceTotal = 0;
+
+  function calculateBill(billString) {
+    var billoutput = billString.split(",");
+    for (var i = 0; i < billoutput.length; i++) {
       var foundBill = billoutput[i].trim();
-      if (foundBill==='sms') {
+      if (foundBill === 'sms') {
         priceSms += 0.75;
-      }
-      else if (foundBill==='call') {
+      } else if (foundBill === 'call') {
         priceCall += 2.75;
       }
-
-
+    }
+    priceTotal = priceSms + priceCall;
+    return priceTotal.toFixed(2);
   }
-  priceTotal = priceSms+priceCall;
 
+  function totalbill() {
+    return priceTotal.toFixed(2);
+  }
 
-return priceTotal;
+  var callPrice = function() {
+    return priceCall.toFixed(2);
+  }
 
+  var smsPrice = function() {
+    return priceSms.toFixed(2);
+  }
+
+  return {
+    billcalc: calculateBill,
+    callprice: callPrice,
+    smsprice: smsPrice,
+    totalprice: totalbill
+  }
 }
+var billcalulate = billCalculate();
 
 
-function calculateBtnClicked(){
-    var billString = billStringElement.value;
-    //round to two decimals
-    var roundedBillTotal = calculateBill(billString).toFixed(2);
+function calculateBtnClicked() {
+  var billString = billStringElement.value;
+  billcalulate.billcalc(billString);
+  roundedBillTotal = billcalulate.totalprice();
 
-    billTotalElement.innerHTML = roundedBillTotal;
+  billTotalElement.innerHTML = roundedBillTotal.toFixed(2);
 
-    if(roundedBillTotal>=20 && roundedBillTotal<30){
+  if (roundedBillTotal >= 20.00 && roundedBillTotal < 30.00) {
 
     billTotalElement.classList.add("warning");
 
-    }
-    else
-     if (roundedBillTotal>=30) {
-      billTotalElement.classList.add("danger");
-    }
+  } else
+  if (roundedBillTotal >= 30.00) {
+    billTotalElement.classList.add("danger");
+  }
 }
 
 //link the function to a click event on the calculate button
-calculateBtn.addEventListener('click', calculateBill);
-calculateBtn.addEventListener('click', calculateBtnClicked);
+//calculateBtn.addEventListener('click', calculateBill);
+calculateBtn.addEventListener('click', calculateBtnClicked){
+}
 
 
 
@@ -66,22 +77,17 @@ calculateBtn.addEventListener('click', calculateBtnClicked);
 
 
 
-
-
-
-
-
-  //split the string
-  // var billItems = billString.split(",");
-  // // a variable for the total phone bill.
-  // var billTotal = 0;
-  // //loop over all the bill items
-  // for (var i=0;i<billItems.length;i++){
-  //     var billItem = billItems[i].trim();
-  //     if (billItem === "call"){
-  //         billTotal += 2.75;
-  //     }
-  //     else if (billItem === "sms"){
-  //         billTotal += 0.75;
-  //     }
-  // }
+//split the string
+// var billItems = billString.split(",");
+// // a variable for the total phone bill.
+// var billTotal = 0;
+// //loop over all the bill items
+// for (var i=0;i<billItems.length;i++){
+//     var billItem = billItems[i].trim();
+//     if (billItem === "call"){
+//         billTotal += 2.75;
+//     }
+//     else if (billItem === "sms"){
+//         billTotal += 0.75;
+//     }
+// }
--- test/calculate-bill.test.js
+++ test/calculate-bill.test.js
@@ -1,15 +1,20 @@
-describe('The calculateBill function', function(){
-    it('should display "0.75" if user entered a string with "sms" only', function(){
-         assert.equal(calculateBill('sms'), 0.75);
-    });
+describe('The calculateBill function', function() {
+  it('should display "0.75" if user entered a string with "sms" only', function() {
+    var billcalulate = billCalculate();
+    billcalulate.billcalc("sms");
+    assert.equal(billcalulate.totalprice(), 0.75);
+  });
 
-    it('should display "2.75" if user entered a string with "call" only', function(){
+  it('should display "2.75" if user entered a string with "call" only', function() {
+    var billcalulate = billCalculate();
+    billcalulate.billcalc("call");
+    assert.equal(billcalulate.totalprice(), 2.75);
+  });
 
-         assert.equal(calculateBill('call'), 2.75);
-    });
-
-    it('should display "3,50" if user entered a string with "sms,call"seperated by comma', function(){
-         assert.equal(calculateBill('sms,call'), 3.50);
-    });
+  it('should display "3,50" if user entered a string with "sms,call"seperated by comma', function() {
+    var billcalulate = billCalculate();
+    billcalulate.billcalc('sms,call');
+    assert.equal(billcalulate.totalprice(), 3.5);
+  });
 
 });
