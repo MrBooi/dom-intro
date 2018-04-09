ce4b91d3c20872f169caae4489a1fcd9ae57ed07
Mr Booi <ayabongabooi2@gmail.com>
2018-04-09 10:52:25 +0200

    done

--- js/calculate-bill.js
+++ js/calculate-bill.js
@@ -6,70 +6,41 @@ var billTotalElement = document.querySelector(".billTotal");
 var billStringElement = document.querySelector(".billString");
 
 
-function billCalculate() {
+function calculateBill(billString) {
   var priceSms = 0;
   var priceCall = 0;
   var priceTotal = 0;
+  var billoutput = billString.split(",");
+  for (var i = 0; i < billoutput.length; i++) {
+    var foundBill = billoutput[i].trim();
 
-  function calculateBill(billString) {
-    var billoutput = billString.split(",");
-    for (var i = 0; i < billoutput.length; i++) {
-      var foundBill = billoutput[i].trim();
-      if (foundBill === 'sms') {
-        priceSms += 0.75;
-      } else if (foundBill === 'call') {
-        priceCall += 2.75;
-      }
+    if (foundBill === 'sms') {
+      priceSms += 0.75;
+    } else if (foundBill === 'call') {
+      priceCall += 2.75;
     }
-    priceTotal = priceSms + priceCall;
-    return priceTotal.toFixed(2);
   }
+  priceTotal = priceSms + priceCall;
 
-  function totalbill() {
-    return priceTotal.toFixed(2);
-  }
+  return priceTotal.toFixed(2);
 
-  var callPrice = function() {
-    return priceCall.toFixed(2);
-  }
-
-  var smsPrice = function() {
-    return priceSms.toFixed(2);
-  }
-
-  return {
-    billcalc: calculateBill,
-    callprice: callPrice,
-    smsprice: smsPrice,
-    totalprice: totalbill
-  }
 }
-var billcalulate = billCalculate();
-
 
 function calculateBtnClicked() {
   var billString = billStringElement.value;
-  billcalulate.billcalc(billString);
-  roundedBillTotal = billcalulate.totalprice();
-
-  billTotalElement.innerHTML = roundedBillTotal.toFixed(2);
+  var roundedBillTotal = calculateBill(billString);
+  billTotalElement.innerHTML = roundedBillTotal;
 
   if (roundedBillTotal >= 20.00 && roundedBillTotal < 30.00) {
-
     billTotalElement.classList.add("warning");
-
-  } else
-  if (roundedBillTotal >= 30.00) {
+  } else if (roundedBillTotal >= 30.00) {
     billTotalElement.classList.add("danger");
   }
-}
 
-//link the function to a click event on the calculate button
-//calculateBtn.addEventListener('click', calculateBill);
-calculateBtn.addEventListener('click', calculateBtnClicked){
 }
 
 
+calculateBtn.addEventListener('click', calculateBtnClicked);
 
 
 
@@ -77,7 +48,8 @@ calculateBtn.addEventListener('click', calculateBtnClicked){
 
 
 
-//split the string
+
+
 // var billItems = billString.split(",");
 // // a variable for the total phone bill.
 // var billTotal = 0;
